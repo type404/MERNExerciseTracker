b38f2e51ae05a80fa842bc112148a3344c7a58f0
Tisa Majumder <coderbychoice01@gmail.com>
2020-04-04 14:30:04 +1100

    Added react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+//install bootstrap npm install bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+//import the components we have created
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercise-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+//install react router npm install react-router-dom it makes it easier to route different url to differnt componenents
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//Below are the components we have to create. 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Navbar/>
+      <br/>
+        <Route path="/" exact component={ExerciseList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUsers extends Component {
+    render() {
+        return (
+            <div>
+                <p>Create Users Component!!</p>
+            </div>
+        )
+    }
+}
